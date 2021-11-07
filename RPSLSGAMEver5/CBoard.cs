ece3735cd317c4875f2c9fa7d86e1f2034fa48e6
Petra Rábai <petra.rabai@icloud.com>
2021-11-07 13:20:44 +0100

    .

--- RPSLSGAMEver5/CBoard.cs
+++ RPSLSGAMEver5/CBoard.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using RPSLSGAMEver5.Properties;
 
 namespace RPSLSGAMEver5
--- RPSLSGAMEver5/CResultSave.cs
+++ RPSLSGAMEver5/CResultSave.cs
@@ -1,10 +1,6 @@
 ﻿using RPSLSGAMEver5.Properties;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RPSLSGAMEver5
 {
